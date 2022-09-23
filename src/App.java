39622f92537c9bc3f89cbd8d0efd16093f5114dd
Luan Dorea <pedroluandm@gmail.com>
2022-09-23 05:33:21 -0300

    Testando o Git

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println();
+        main();
+    }
+
+    public static void main() {
+    }
+
+    public static void main(int a) {
+    }
+}
